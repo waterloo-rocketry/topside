ba4ff69f4c4da7bb5532f2280de49741d446e928
Jacob Deery <jacob.deery@gmail.com>
2020-04-06 12:53:47 -0400

    Better separate application and library code (#3)
    Move all application code into a new top level "application" directory,
    and put the "resources" folder into that directory as well so that we
    can use it to store resources Qt needs (like the icon).

---
+++ application/main.py
@@ -0,0 +1,38 @@
+from os import path
+import sys
+
+from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtGui import QIcon
+
+import operations_simulator as ops
+
+
+# NOTE(jacob): `__file__` isn't defined for the frozen application,
+# so we need to use this function for finding resources in relative
+# paths. See below:
+# cx-freeze.readthedocs.io/en/latest/faq.html#using-data-files
+def find_resource(filename):
+    if getattr(sys, 'frozen', False):
+        datadir = path.dirname(sys.executable)
+    else:
+        datadir = path.dirname(__file__)
+    return path.join(datadir, 'resources', filename)
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setWindowTitle('Operations Simulator')
+        self.setWindowIcon(QIcon(find_resource('icon.ico')))
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    plumb = ops.PlumbingEngine()
+
+    window = Window()
+    window.resize(1200, 800)
+    window.show()
+
+    sys.exit(app.exec_())
