24391caacb827e48a83853fdfd276b0f53bb4eb3
Jacob Deery <jacob.deery@gmail.com>
2020-04-06 23:46:11 -0400

    Add error message if autopep8 is missing

--- format.sh
+++ format.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
+if ! type autopep8 &> /dev/null
+then
+    echo "Error: autopep8 is not installed."
+    echo "Please run 'pip install autopep8'."
+    exit 1
+fi
+
 WORKSPACE_DIR=$(git rev-parse --show-toplevel)
 cd "$WORKSPACE_DIR"
 
