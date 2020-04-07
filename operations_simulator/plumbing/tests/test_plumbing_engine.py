6f9c6dbab6f4a4f73f3ee66ec690045a448c395f
Wendi Yu <w75yu@edu.uwaterloo.ca>
2020-04-06 19:48:28 -0600

    Set up constructors for each class
    Constructors for PlumbingEngine and PlumbingComponent classes, as well
    as function to load in graphs. Basic functionality works but haven't
    considered edge cases, invalid input, etc. yet.

--- operations_simulator/plumbing/tests/test_plumbing_engine.py
+++ operations_simulator/plumbing/tests/test_plumbing_engine.py
@@ -4,3 +4,71 @@ import operations_simulator as ops
 def test_plumbing_engine_exists():
     plumb = ops.PlumbingEngine()
     assert plumb is not None
+
+def test_plumbing_component_exists():
+    plumb_comp = ops.PlumbingComponent('', {}, [])
+    assert plumb_comp is not None
+
+def test_plumbing_component_setup():
+    pc1_states = {
+    'open' : {
+        (1, 2, 'A1') : 0.001,
+        (2, 1, 'A2') : 0.001
+        },
+    'closed' : {
+        (1, 2, 'A1') : 1000,
+        (2, 1, 'A2') : 1000
+        }
+    }
+    pc1_edges = [(1, 2, 'A1'), (2, 1, 'A2')]
+    pc1 = ops.PlumbingComponent('valve1', pc1_states, pc1_edges)
+
+    assert pc1.component_graph.edges(keys=True) == [(1, 2, 'A1'), (2, 1, 'A2')]
+    assert pc1.component_graph.nodes() == [1, 2]
+    assert pc1.states == {'open': {(1, 2, 'A1'): 4500.0, (2, 1, 'A2'): 4500.0}, 'closed': {(1, 2, 'A1'): 0.0045, (2, 1, 'A2'): 0.0045}}
+
+def test_plumbing_engine_setup():
+    pc1_states = {
+    'open' : {
+        (1, 2, 'A1') : 0.001,
+        (2, 1, 'A2') : 0.001
+        },
+    'closed' : {
+        (1, 2, 'A1') : 1000,
+        (2, 1, 'A2') : 1000
+        }
+    }
+    pc1_edges = [(1, 2, 'A1'), (2, 1, 'A2')]
+    pc1 = ops.PlumbingComponent('valve1', pc1_states, pc1_edges)
+
+    pc2_states = {
+    'open' : {
+        (1, 2, 'B1') : 0.001,
+        (2, 1, 'B2') : 0.001
+        },
+    'closed' : {
+        (1, 2, 'B1') : 1000,
+        (2, 1, 'B2') : 1000
+        }
+    }
+    pc2_edges = [(1, 2, 'B1'), (2, 1, 'B2')]
+    pc2 = ops.PlumbingComponent('valve2', pc2_states, pc2_edges)
+
+    component_mapping = {
+        'valve1' : {
+            1 : 1,
+            2 : 2
+        },
+        'valve2' : {
+            1 : 2,
+            2 : 3
+        }
+    }
+
+    pressures = {3 : 100}
+    default_states = {'valve1' : 'closed', 'valve2' : 'open'}
+    plumb = ops.PlumbingEngine([pc1, pc2], component_mapping, pressures, default_states)
+
+    assert plumb.plumbing_graph.edges(data=True, keys=True) == [(1, 2, 'A1', {'FC': 0.0045}), (2, 1, 'A2', {'FC': 0.0045}), (2, 3, 'B1', {'FC': 4500.0}), (3, 2, 'B2', {'FC': 4500.0})]
+    assert plumb.time_resolution == 5e-05
+    assert plumb.plumbing_graph.nodes(data=True) == [(1, {'pressure': 0}), (2, {'pressure': 0}), (3, {'pressure': 100})]
