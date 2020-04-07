44401f4f60020323265e8b381e34235606afad3c
Wendi Yu <w75yu@edu.uwaterloo.ca>
2020-04-06 22:14:48 -0600

    Move functionality to helper functions
    _set_time_resolution() and toggle_component() now handle some
    reusable constructor functionality

--- operations_simulator/plumbing/tests/test_plumbing_engine.py
+++ operations_simulator/plumbing/tests/test_plumbing_engine.py
@@ -16,8 +16,8 @@ def test_plumbing_component_setup():
         (2, 1, 'A2') : 0.001
         },
     'closed' : {
-        (1, 2, 'A1') : 1000,
-        (2, 1, 'A2') : 1000
+        (1, 2, 'A1') : 'closed',
+        (2, 1, 'A2') : 'closed'
         }
     }
     pc1_edges = [(1, 2, 'A1'), (2, 1, 'A2')]
@@ -30,12 +30,12 @@ def test_plumbing_component_setup():
 def test_plumbing_engine_setup():
     pc1_states = {
     'open' : {
-        (1, 2, 'A1') : 0.001,
-        (2, 1, 'A2') : 0.001
+        (1, 2, 'A1') : 0,
+        (2, 1, 'A2') : 0
         },
     'closed' : {
-        (1, 2, 'A1') : 1000,
-        (2, 1, 'A2') : 1000
+        (1, 2, 'A1') : 'closed',
+        (2, 1, 'A2') : 'closed'
         }
     }
     pc1_edges = [(1, 2, 'A1'), (2, 1, 'A2')]
@@ -43,12 +43,12 @@ def test_plumbing_engine_setup():
 
     pc2_states = {
     'open' : {
-        (1, 2, 'B1') : 0.001,
-        (2, 1, 'B2') : 0.001
+        (1, 2, 'B1') : 0,
+        (2, 1, 'B2') : 0
         },
     'closed' : {
-        (1, 2, 'B1') : 1000,
-        (2, 1, 'B2') : 1000
+        (1, 2, 'B1') : 'closed',
+        (2, 1, 'B2') : 'closed'
         }
     }
     pc2_edges = [(1, 2, 'B1'), (2, 1, 'B2')]
@@ -69,6 +69,6 @@ def test_plumbing_engine_setup():
     default_states = {'valve1' : 'closed', 'valve2' : 'open'}
     plumb = ops.PlumbingEngine([pc1, pc2], component_mapping, pressures, default_states)
 
-    assert plumb.plumbing_graph.edges(data=True, keys=True) == [(1, 2, 'A1', {'FC': 0.0045}), (2, 1, 'A2', {'FC': 0.0045}), (2, 3, 'B1', {'FC': 4500.0}), (3, 2, 'B2', {'FC': 4500.0})]
+    assert plumb.plumbing_graph.edges(data=True, keys=True) == [(1, 2, 'A1', {'FC': 0}), (2, 1, 'A2', {'FC': 0}), (2, 3, 'B1', {'FC': 10}), (3, 2, 'B2', {'FC': 10})]
     assert plumb.time_resolution == 5e-05
     assert plumb.plumbing_graph.nodes(data=True) == [(1, {'pressure': 0}), (2, {'pressure': 0}), (3, {'pressure': 100})]
