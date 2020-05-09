774b5e6df1b62e226293ef2ec3f5f3e2e02f5c13
Jacob Deery <jacob.deery@gmail.com>
2020-05-09 00:02:01 -0400

    Remove unnecessary import and fix capitalization

--- topside/visualization/layout.py
+++ topside/visualization/layout.py
@@ -3,14 +3,12 @@ from dataclasses import dataclass
 import numpy as np
 import networkx as nx
 
-from topside import PlumbingEngine, PlumbingComponent
-
 
 def terminal_graph(plumbing_engine):
-    T = nx.Graph()
+    t = nx.Graph()
 
     non_atm_nodes = [n for n in plumbing_engine.plumbing_graph if not n == 'atm']
-    T.add_nodes_from(non_atm_nodes)
+    t.add_nodes_from(non_atm_nodes)
 
     component_nodes = {}
 
@@ -18,10 +16,10 @@ def terminal_graph(plumbing_engine):
         component_nodes[name] = []
         for edge in c.component_graph.edges:
             (n1, n2, edge_id) = edge
-            T.add_edge(name + '.' + str(n1), name + '.' + str(n2))
+            t.add_edge(name + '.' + str(n1), name + '.' + str(n2))
         for node in c.component_graph.nodes:
             component_nodes[name].append(name + '.' + str(node))
             if not plumbing_engine.mapping[name][node] == 'atm':
-                T.add_edge(name + '.' + str(node), plumbing_engine.mapping[name][node])
+                t.add_edge(name + '.' + str(node), plumbing_engine.mapping[name][node])
 
-    return T, non_atm_nodes, component_nodes
+    return t, non_atm_nodes, component_nodes
