509889ed7725ab40aedd6f9449842f17ece3eeeb
Jacob Deery <jacob.deery@gmail.com>
2020-05-25 21:16:17 -0400

    Fix the format script to work on untracked files
    git diff --name-only master doesn't include untracked files, so the
    format script wasn't actually doing anything to them. Add another line
    to include these files as well.

--- tools/format.sh
+++ tools/format.sh
@@ -10,7 +10,7 @@ fi
 WORKSPACE_DIR=$(git rev-parse --show-toplevel)
 cd "$WORKSPACE_DIR"
 
-FILES=$(git diff --name-only master | grep '\.py$')
+FILES=$({ git ls-files -o --exclude-standard && git diff --name-only master; } | grep '\.py$')
 
 if [ ! -z "$FILES" ]
 then
