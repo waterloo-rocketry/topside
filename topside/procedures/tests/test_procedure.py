5cd29e473d539dcf26be0847d88ab5e353e0324a
Jacob Deery <jacob.deery@gmail.com>
2020-09-13 16:31:17 -0400

    proc: Add ProcedureSuite class (#56)
    New class that provides a convenient way for grouping a set of related
    procedures and associated metadata. Currently the only metadata is the
    entry point into the suite (the first procedure that should be
    executed), but we also add checks for duplicate procedure IDs, fixing a
    potential bug.

--- topside/procedures/tests/test_procedure.py
+++ topside/procedures/tests/test_procedure.py
@@ -75,3 +75,137 @@ def test_procedure_equality_different_steps():
     proc_2 = top.Procedure('p1', [s1, s3])
 
     assert proc_1 != proc_2
+
+
+def test_procedure_suite_builds_list():
+    s1 = top.ProcedureStep('s1', None, [])
+    s2 = top.ProcedureStep('s2', None, [])
+    s3 = top.ProcedureStep('s3', None, [])
+
+    proc_1 = top.Procedure('p1', [s1, s2])
+    proc_2 = top.Procedure('p2', [s1, s3])
+
+    suite = top.ProcedureSuite([proc_1, proc_2], 'p1')
+
+    assert suite.procedures == {'p1': proc_1, 'p2': proc_2}
+
+
+def test_procedure_suite_sets_starting_procedure():
+    s1 = top.ProcedureStep('s1', None, [])
+    s2 = top.ProcedureStep('s2', None, [])
+    s3 = top.ProcedureStep('s3', None, [])
+
+    main = top.Procedure('main', [s1, s2, s3])
+    not_main = top.Procedure('not_main', [s1, s2, s3])
+
+    suite = top.ProcedureSuite([main, not_main], 'not_main')
+    assert suite.starting_procedure_id == 'not_main'
+
+
+def test_procedure_suite_defaults_to_main():
+    s1 = top.ProcedureStep('s1', None, [])
+    s2 = top.ProcedureStep('s2', None, [])
+    s3 = top.ProcedureStep('s3', None, [])
+
+    main = top.Procedure('main', [s1, s2, s3])
+    not_main = top.Procedure('not_main', [s1, s2, s3])
+
+    suite = top.ProcedureSuite([main, not_main])
+    assert suite.starting_procedure_id == 'main'
+
+
+def test_procedure_suite_duplicate_name_errors():
+    s1 = top.ProcedureStep('s1', None, [])
+    s2 = top.ProcedureStep('s2', None, [])
+    s3 = top.ProcedureStep('s3', None, [])
+
+    proc_1 = top.Procedure('p1', [s1, s2])
+    proc_2 = top.Procedure('p1', [s1, s3])
+
+    with pytest.raises(Exception):
+        top.ProcedureSuite([proc_1, proc_2])
+
+
+def test_procedure_suite_no_procedures_errors():
+    with pytest.raises(Exception):
+        top.ProcedureSuite([])
+
+
+def test_procedure_suite_invalid_starting_procedure_errors():
+    s1 = top.ProcedureStep('s1', None, [])
+    s2 = top.ProcedureStep('s2', None, [])
+    s3 = top.ProcedureStep('s3', None, [])
+
+    p1 = top.Procedure('p1', [s1, s2, s3])
+
+    with pytest.raises(Exception):
+        top.ProcedureSuite([p1], 'p2')
+
+
+def test_procedure_suite_equality_equal():
+    s1 = top.ProcedureStep('s1', None, [])
+    s2 = top.ProcedureStep('s2', None, [])
+    s3 = top.ProcedureStep('s3', None, [])
+
+    proc_1 = top.Procedure('p1', [s1, s2])
+    proc_2 = top.Procedure('p2', [s1, s3])
+
+    suite_1 = top.ProcedureSuite([proc_1, proc_2], 'p1')
+    suite_2 = top.ProcedureSuite([proc_1, proc_2], 'p1')
+
+    assert suite_1 == suite_2
+
+
+def test_procedure_suite_equality_order_irrelevant():
+    s1 = top.ProcedureStep('s1', None, [])
+    s2 = top.ProcedureStep('s2', None, [])
+    s3 = top.ProcedureStep('s3', None, [])
+
+    proc_1 = top.Procedure('p1', [s1, s2])
+    proc_2 = top.Procedure('p2', [s1, s3])
+
+    suite_1 = top.ProcedureSuite([proc_1, proc_2], 'p1')
+    suite_2 = top.ProcedureSuite([proc_2, proc_1], 'p1')
+
+    assert suite_1 == suite_2
+
+
+def test_procedure_suite_equality_different_starting_procedure():
+    s1 = top.ProcedureStep('s1', None, [])
+    s2 = top.ProcedureStep('s2', None, [])
+    s3 = top.ProcedureStep('s3', None, [])
+
+    proc_1 = top.Procedure('p1', [s1, s2])
+    proc_2 = top.Procedure('p2', [s1, s3])
+
+    suite_1 = top.ProcedureSuite([proc_1, proc_2], 'p1')
+    suite_2 = top.ProcedureSuite([proc_1, proc_2], 'p2')
+
+    assert suite_1 != suite_2
+
+
+def test_procedure_suite_equality_different_procedures():
+    s1 = top.ProcedureStep('s1', None, [])
+    s2 = top.ProcedureStep('s2', None, [])
+    s3 = top.ProcedureStep('s3', None, [])
+
+    proc_1 = top.Procedure('p1', [s1, s2])
+    proc_2 = top.Procedure('p2', [s1, s3])
+
+    suite_1 = top.ProcedureSuite([proc_1], 'p1')
+    suite_2 = top.ProcedureSuite([proc_1, proc_2], 'p1')
+
+    assert suite_1 != suite_2
+
+
+def test_procedure_suite_indexing():
+    s1 = top.ProcedureStep('s1', None, [])
+    s2 = top.ProcedureStep('s2', None, [])
+    s3 = top.ProcedureStep('s3', None, [])
+
+    proc_1 = top.Procedure('p1', [s1, s2])
+    proc_2 = top.Procedure('p2', [s1, s3])
+
+    suite = top.ProcedureSuite([proc_1, proc_2], 'p1')
+
+    assert suite['p1'] == proc_1
