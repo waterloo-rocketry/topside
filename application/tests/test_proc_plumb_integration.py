f6ca23914f675156eabf2eb985e96a7af7452a00
Jacob Deery <jacob.deery@gmail.com>
2020-10-19 10:58:12 -0400

    app: Add tests for Topside bridge code (#88)
    The PlumbingBridge and ProceduresBridge classes didn't have any unit
    tests written for them, which is very bad. This commit adds unit tests for
    both classes. It also splits off some of the procedure wrapper code into
    a separate file, since procedures_bridge.py was starting to get a bit
    too long.

---
+++ application/tests/test_proc_plumb_integration.py
@@ -0,0 +1,141 @@
+import textwrap
+
+import topside as top
+from ..procedures_bridge import ProceduresBridge
+from ..plumbing_bridge import PlumbingBridge
+
+
+def make_plumbing_engine():
+    pdl = textwrap.dedent("""\
+    name: example
+
+    body:
+    - component:
+        name: injector_valve
+        edges:
+          edge1:
+              nodes: [A, B]
+        states:
+          open:
+              edge1: 1
+          closed:
+              edge1: closed
+    - graph:
+        name: main
+        nodes:
+          A:
+            initial_pressure: 100
+            components:
+              - [injector_valve, A]
+          B:
+            initial_pressure: 0
+            components:
+              - [injector_valve, B]
+        states:
+          injector_valve: closed
+    """)
+    parser = top.Parser([pdl], 's')
+    return parser.make_engine()
+
+
+def test_proc_bridge_procedure_step_affects_plumbing():
+    plumb_b = PlumbingBridge()
+    proc_b = ProceduresBridge(plumb_b)
+
+    procedure = top.Procedure('main', [
+        top.ProcedureStep('0', top.MiscAction('Start of operations'), [
+            (top.Immediate(), top.Transition('main', '1'))], 'OPS'),
+        top.ProcedureStep('1', top.StateChangeAction('injector_valve', 'open'), [
+            (top.Immediate(), top.Transition('main', '2'))], 'PRIMARY'),
+        top.ProcedureStep('2', top.StateChangeAction('injector_valve', 'closed'), [
+            (top.Immediate(), top.Transition('main', '3'))], 'PRIMARY'),
+        top.ProcedureStep('3', top.MiscAction('Approach the launch tower'), [], 'SECONDARY')
+    ])
+    proc_b.load_suite(top.ProcedureSuite([procedure]))
+
+    plumb_eng = make_plumbing_engine()
+    plumb_b.load_engine(plumb_eng)
+
+    assert plumb_eng.current_state('injector_valve') == 'closed'
+    proc_b.procStepForward()
+    assert plumb_eng.current_state('injector_valve') == 'open'
+    proc_b.procStepForward()
+    assert plumb_eng.current_state('injector_valve') == 'closed'
+
+
+def test_time_step_forward():
+    plumb_b = PlumbingBridge()
+    proc_b = ProceduresBridge(plumb_b)
+
+    procedure = top.Procedure('main', [
+        top.ProcedureStep('0', top.MiscAction('Start of operations'), [
+            (top.Immediate(), top.Transition('main', '1'))], 'OPS'),
+        top.ProcedureStep('1', top.StateChangeAction('injector_valve', 'open'), [
+            (top.WaitUntil(0.2e6), top.Transition('main', '2'))], 'PRIMARY'),
+        top.ProcedureStep('2', top.StateChangeAction('injector_valve', 'closed'), [
+            (top.WaitUntil(0.4e6), top.Transition('main', '3'))], 'PRIMARY'),
+        top.ProcedureStep('3', top.MiscAction('Approach the launch tower'), [], 'SECONDARY')
+    ])
+    proc_b.load_suite(top.ProcedureSuite([procedure]))
+
+    plumb_eng = make_plumbing_engine()
+    plumb_b.load_engine(plumb_eng)
+
+    proc_eng = proc_b._proc_eng
+
+    proc_b.procStepForward()  # advance to step 1
+    assert not proc_eng.ready_to_advance()
+    # Time hasn't advanced yet, so pressures should be the same
+    assert plumb_eng.current_pressures('A') == 100
+    assert plumb_eng.current_pressures('B') == 0
+
+    proc_b.timeStepForward()  # time == 0.1s
+    assert not proc_eng.ready_to_advance()
+    # Valve is open, so pressures should start dropping
+    assert plumb_eng.current_pressures('A') < 100
+    assert plumb_eng.current_pressures('B') > 0
+
+    proc_b.timeStepForward()  # time == 0.2s
+    assert proc_eng.ready_to_advance()
+    pressures_at_02 = plumb_eng.current_pressures()
+
+    proc_b.procStepForward()  # advance to step 2
+    assert not proc_eng.ready_to_advance()
+    assert plumb_eng.current_pressures() == pressures_at_02
+
+    proc_b.timeStepForward()  # time == 0.3s
+    assert not proc_eng.ready_to_advance()
+    # Valve is now closed, so pressures shouldn't change further
+    assert plumb_eng.current_pressures() == pressures_at_02
+
+
+def test_time_advance():
+    plumb_b = PlumbingBridge()
+    proc_b = ProceduresBridge(plumb_b)
+
+    procedure = top.Procedure('main', [
+        top.ProcedureStep('0', top.MiscAction('Start of operations'), [
+            (top.Immediate(), top.Transition('main', '1'))], 'OPS'),
+        top.ProcedureStep('1', top.StateChangeAction('injector_valve', 'open'), [
+            (top.Less('A', 75), top.Transition('main', '2'))], 'PRIMARY'),
+        top.ProcedureStep('2', top.MiscAction('Approach the launch tower'), [], 'SECONDARY')
+    ])
+    proc_b.load_suite(top.ProcedureSuite([procedure]))
+
+    plumb_eng = make_plumbing_engine()
+    plumb_b.load_engine(plumb_eng)
+
+    tol = 0.01
+
+    proc_eng = proc_b._proc_eng
+
+    proc_b.procStepForward()  # advance to step 1
+    assert not proc_eng.ready_to_advance()
+    assert plumb_eng.current_pressures('A') == 100
+    assert plumb_eng.current_pressures('B') == 0
+
+    proc_b.timeAdvance()
+    assert proc_eng.ready_to_advance()
+    # We expect pressures to equalize at 50 once the system is steady
+    assert abs(plumb_eng.current_pressures('A') - 50) < tol
+    assert abs(plumb_eng.current_pressures('B') - 50) < tol
---
+++ application/tests/test_procedures_bridge.py
@@ -0,0 +1,63 @@
+from PySide2.QtCore import QObject, Signal
+
+import topside as top
+from ..procedures_bridge import ProceduresBridge
+
+
+class MockPlumbingBridge(QObject):
+    engineLoaded = Signal(top.PlumbingEngine)
+
+    def __init__(self):
+        QObject.__init__(self)
+        self.engine = None
+
+
+def test_proc_bridge_init():
+    plumb_b = MockPlumbingBridge()
+    proc_b = ProceduresBridge(plumb_b)
+
+    assert proc_b._proc_eng._suite is None
+    assert proc_b._proc_steps.procedure is None
+
+
+def test_proc_bridge_load_suite():
+    plumb_b = MockPlumbingBridge()
+    proc_b = ProceduresBridge(plumb_b)
+
+    procedure = top.Procedure('main', [
+        top.ProcedureStep('1', top.StateChangeAction('injector_valve', 'open'), [
+            (top.Immediate(), top.Transition('main', '2'))], 'PRIMARY'),
+        top.ProcedureStep('2', top.MiscAction('Approach the launch tower'), [], 'SECONDARY')
+    ])
+
+    suite = top.ProcedureSuite([procedure])
+
+    proc_b.load_suite(suite)
+
+    assert proc_b._proc_eng._suite == suite
+    assert proc_b.steps.procedure == procedure
+
+
+def test_proc_bridge_procedure_controls_advance_procedure():
+    plumb_b = MockPlumbingBridge()
+    proc_b = ProceduresBridge(plumb_b)
+
+    procedure = top.Procedure('main', [
+        top.ProcedureStep('1', top.StateChangeAction('injector_valve', 'open'), [
+            (top.Immediate(), top.Transition('main', '2'))], 'PRIMARY'),
+        top.ProcedureStep('2', top.StateChangeAction('vent_valve', 'closed'), [
+            (top.WaitUntil(100), top.Transition('main', '3'))], 'PRIMARY'),
+        top.ProcedureStep('3', top.MiscAction('Approach the launch tower'), [], 'SECONDARY')
+    ])
+
+    proc_b.load_suite(top.ProcedureSuite([procedure]))
+
+    proc_eng = proc_b._proc_eng
+
+    assert proc_eng.current_step == procedure.step_list[0]
+    proc_b.procStepForward()
+    assert proc_eng.current_step == procedure.step_list[1]
+    proc_b.procStepForward()  # does nothing; condition is not satisfied
+    assert proc_eng.current_step == procedure.step_list[1]
+    proc_b.procStop()
+    assert proc_eng.current_step == procedure.step_list[0]
