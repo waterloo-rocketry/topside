5c078f739900ef919aa544919b06ab8d0cbc7336
Ximing Yang <james020118@gmail.com>
2020-11-09 21:50:05 -0500

    fixed issues regarding the PR

--- topside/procedures/tests/run_simulation.py
+++ topside/procedures/tests/run_simulation.py
@@ -1,37 +1,33 @@
 import argparse
 
+import matplotlib.pyplot as plt
+
 import topside as top
 
-import matplotlib.pyplot as plt
-import topside.plumbing.plumbing_utils as plumbing_utils
 
-
-# def make_plumbing_engine(components, mapping, initial_pressures, initial_states):
-#     plumb = top.PlumbingEngine()
-#     plumb.load_graph(components, mapping, initial_pressures, initial_states)
-#     return plumb
-
-
-def make_procedures_engine(plumbing_eng, suite):
+def generate_graph_data(pressures, time_res, nodes):
     """
-    makes a new procedures enging.
-    """
-    procedures_eng = top.ProceduresEngine(plumbing_eng, suite)
-    return procedures_eng
+    Generates the data needed for graphing the pressure.
 
+    Parameters
+    ----------
 
-def make_plot(pressures, time_res, nodes):
-    """
-    makes a graph based on the logged time and pressure of each node.
+    pressures: 
+        all pressures logged while running the procedures.
+
+    time_res:  
+        time interval for the plumbing engine.
+
+    nodes: 
+        all nodes in the plumbing engine.
     """
     node_pressures = {node: [] for node in nodes}
-
     t = []
     time = 0
 
     for p in pressures:
         t.append(time)
-        time += plumbing_utils.micros_to_s(time_res)
+        time += top.micros_to_s(time_res)
 
         for node, pres in p.items():
             node_pressures[node].append(pres)
@@ -39,56 +35,41 @@ def make_plot(pressures, time_res, nodes):
     return t, node_pressures
 
 
-# read file paths from argument input
-# first file path: Proclang file path
-# second file path: Pdl file path
 arg_parser = argparse.ArgumentParser(
     description="A program that simulates a procedure from given plumbing engine and procedure suite.")
-arg_parser.add_argument('procPath', type=str)
-arg_parser.add_argument('pdlPath', type=str)
+arg_parser.add_argument('proc_path', type=str, help='Path to the procLang file')
+arg_parser.add_argument('pdl_path', type=str, help='Path to the pdl file')
+arg_parser.add_argument('max_time', type=int, help='max time allowed for the simulation in seconds')
 
 args = arg_parser.parse_args()
 
-# parsing file paths and make plumbing engine
-plumb_parsed = top.Parser([args.pdlPath])
+plumb_parsed = top.Parser([args.pdl_path])
 plumb = plumb_parsed.make_engine()
 
 assert plumb.is_valid()
-
-suite = top.proclang.parse_from_file(args.procPath)
-
-assert suite != None
-
-# construct procedures engine from given plumbing engine
-procedures_eng = make_procedures_engine(plumb, suite)
+suite = top.proclang.parse_from_file(args.proc_path)
+assert suite is not None
+procedures_eng = top.ProceduresEngine(plumb, suite)
 
 all_pressure = []
 
-# 1. the loop runs until the end of procedure
-# 2. check to see if the procedures engine is ready to advance to the next step (ready_to_advance)
-# 3. if so, advance one step (next_step)
-# 4. step forward in time by a small amount (step_time)
-# 5. record the current pressures of the system
-# 6. repeat steps 2-5 until we are at the end of the procedure
-
-while len(procedures_eng.current_step.conditions) > 0:
+while len(procedures_eng.current_step.conditions) > 0 and plumb.time < top.s_to_micros(args.max_time):
     if procedures_eng.ready_to_advance():
         procedures_eng.next_step()
     procedures_eng.step_time()
-    all_pressure.append(procedures_eng._plumb.current_pressures())
+    all_pressure.append(plumb.current_pressures())
 
-tlist, node_plist = make_plot(all_pressure, procedures_eng._plumb.time_res,
-                              procedures_eng._plumb.nodes(data=False))
+tlist, node_plist = generate_graph_data(all_pressure, plumb.time_res,
+                                        plumb.nodes(data=False))
 
 for name, pressure in node_plist.items():
     plt.plot(tlist, pressure, label='node ' + str(name))
 
 plt.xlabel('Time (s)')
-plt.ylabel('Pressure (Pa)')
-plt.suptitle('Pressure Simulation')
+plt.ylabel('Pressure (psi)')
+plt.suptitle('Procedure Simulation')
 plt.grid(color='lightgrey')
 plt.xlim(0, tlist[-1])
-plt.ylim(-1, 101)
 
 plt.legend()
 plt.show()
